32711c1929da7aac85d8d2e7a20204e2fd942b11
duimaurisfootball <gabrieldavis211@gmail.com>
2025-01-23 15:52:16 -0500

    Add app.js functionality, add server.js functionality

--- books-to-read/server/server.js
+++ books-to-read/server/server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
@@ -6,3 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   });
+=======
+import app from './app';
+
+app.listen(3000, () => console.log('The server is listening on port 3000'))
+>>>>>>> 933b70b (Add app.js functionality, add server.js functionality)
