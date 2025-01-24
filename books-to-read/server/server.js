d4a55838afcfa13913be14eb0581ffca742aff97
Oliver Antonio Fajardo <oliverafajardoucb@gmail.com>
2025-01-23 17:35:16 -0800

    Remove README.md

--- books-to-read/server/server.js
+++ books-to-read/server/server.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import app from './app.js';
-
-const PORT = process.env.PORT || 3000;
-
-// app.listen(3000, () => console.log('Server is listening on port 3000'));
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
-=======
 import app from './app';
 
 app.listen(3000, () => console.log('The server is listening on port 3000'))
->>>>>>> 933b70b (Add app.js functionality, add server.js functionality)
